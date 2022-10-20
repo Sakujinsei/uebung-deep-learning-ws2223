ba33ae03783cb6b79e1980086396e5808437b551
thomas5chneider <115982322+thomas5chneider@users.noreply.github.com>
2022-10-20 15:19:50 +0200

    Update calc.py
    Updated per instructions

--- exercise-02/calc.py
+++ exercise-02/calc.py
@@ -1,6 +1,18 @@
-print("Enter number 1: ")
-num1 = int(input())
-print("Enter number 2: ")
-num2 = int(input())
-print("Sum: " + str(num1+num2))
-print("Product: " + str(num1*num2))
+go = True
+while go:
+ print("Enter number 1: ")
+ num1 = int(input())
+ print("Enter number 2: ")
+ num2 = int(input())
+ print("Select function: (a)dd or (m)ultiply")
+ sel = str(input())
+ if sel == "a" or sel == "add" or sel == "addition":
+  print("Sum: " + str(num1+num2))
+ elif sel == "m" or sel == "multi" or sel == "multiplication":
+  print("Product: " + str(num1*num2))
+ print("Go again - (y)es or no? ")
+ rep = str(input())
+ if rep == "y" or rep =="yes":
+    go = True
+ else:
+    go = False
