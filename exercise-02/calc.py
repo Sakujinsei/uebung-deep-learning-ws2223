2834d86f212ea773c11221b461b6d7e2dfcff048
DKramer8 <103896137+DKramer8@users.noreply.github.com>
2022-10-20 13:18:31 +0200

    Fertig

--- exercise-02/calc.py
+++ exercise-02/calc.py
@@ -1,6 +1,21 @@
-print("Enter number 1: ")
-num1 = int(input())
-print("Enter number 2: ")
-num2 = int(input())
-print("Sum: " + str(num1+num2))
-print("Product: " + str(num1*num2))
+def interact():
+	print("Enter number 1: ")
+	num1 = int(input())
+	print("Enter number 2: ")
+	num2 = int(input())
+
+	print("Choose operation: add or multiply?")
+	answer = input()
+	if answer == "add" or answer == "addition" or answer == "Add" or answer == "Addition":
+		print("Sum: " + str(num1+num2))
+	elif answer == "multiply" or answer == "multiplication" or answer == "Multiply" or answer == "Multiplication":
+		print("Product: " + str(num1*num2))
+	else:
+		print("Invalid operation!")
+
+	print("Continue? Yes/No?")
+	if input() == "Yes":
+		interact()
+
+
+interact()
