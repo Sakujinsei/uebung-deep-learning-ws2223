39305ec5a1ea4dbdc0b6f112d1daa4f72e1834e2
Julius Roschke <juro2015@web.de>
2022-10-20 21:38:46 +0200

    Task solved

--- exercise-02/calc.py
+++ exercise-02/calc.py
@@ -1,6 +1,26 @@
-print("Enter number 1: ")
-num1 = int(input())
-print("Enter number 2: ")
-num2 = int(input())
-print("Sum: " + str(num1+num2))
-print("Product: " + str(num1*num2))
+while True:
+    #Zahlen innput
+    print("Enter number 1: ")
+    num1 = int(input())
+    print("Enter number 2: ")
+    num2 = int(input())
+
+    #Rechenoperation
+    print("Rechenoperation[+*]")
+    op=input()
+    if op=="+":
+        print("Sum: " + str(num1+num2))
+    elif op=="*":
+        print("Product: " + str(num1*num2))
+    else:
+        print("Eingabe fehlerhaft")
+    
+    #Wiederholung
+    print("Neustart?[y/n]")
+    ns=input()
+    if ns=="n":
+        break
+    elif ns=="y":
+        print("Neustart")
+    else:
+        print("Eingabe fehlerhaft")
