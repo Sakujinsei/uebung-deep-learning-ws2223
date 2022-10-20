79c5a9d1a6ca2fe5fa5d4cf5fcbebf6791ab6110
David Müller <Dreevid@web.de>
2022-10-20 13:07:05 +0200

    Aufgabe fertig

--- exercise-02/calc.py
+++ exercise-02/calc.py
@@ -1,6 +1,16 @@
-print("Enter number 1: ")
-num1 = int(input())
-print("Enter number 2: ")
-num2 = int(input())
-print("Sum: " + str(num1+num2))
-print("Product: " + str(num1*num2))
+x = "yes"
+while x == "yes":
+    print("Enter number 1: ")
+    num1 = int(input())
+    print("Enter number 2: ")
+    num2 = int(input())
+    print("Enter operator (multiply or add): ")
+    operation = input()
+
+    if(operation == "multiply" or operation == "*" or operation == "multi"):
+       print("Product:", str(num1*num2))
+    if(operation == "add" or operation == "+" or operation == "addition"):
+        print("Sum: " + str(num1+num2))
+    print("Continue? yes/no: ")
+    x = input()
+
