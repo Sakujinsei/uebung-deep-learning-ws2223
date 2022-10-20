19f4b94146adbc54a5fa97e10334c01a65b8f8c7
bjenne <bjenne@smail.uni-koeln.de>
2022-10-20 15:41:17 +0200

    changed
    changed

--- exercise-02/calc.py
+++ exercise-02/calc.py
@@ -1,6 +1,31 @@
-print("Enter number 1: ")
+def func(sum): + str(num1+num2)
+def func(pro): + str(num1*num2)
+
+
+start: print("Enter number 1: ")
 num1 = int(input())
 print("Enter number 2: ")
 num2 = int(input())
-print("Sum: " + str(num1+num2))
-print("Product: " + str(num1*num2))
+give = input("sum or pro")
+if sum:
+    print("sum: " + str(num1+num2))
+elif pro:
+    print("pro: " + str(num1*num2))
+
+answer = input ('you want to exit?: [y/n]')
+if answer[0].lower() == 'n':
+    print("Restart")
+    print("Enter number 1: ")
+    num1 = int(input())
+    print("Enter number 2: ")
+    num2 = int(input())
+    give = input("sum or pro")
+    if sum:
+        print("sum: " + str(num1+num2))
+    elif pro:
+        print("pro: " + str(num1*num2))
+
+if answer[0].lower == 'y':
+    exit()
+# for yes in range(0, 0):
+ #   print("sorry" + exit())
