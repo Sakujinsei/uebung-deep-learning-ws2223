4f303f88b1fd944c64d8424f190a44a1916d19cd
dr123v <dr123v@gmail.com>
2022-10-20 18:17:22 +0200

    COMMENT

--- exercise-02/calc.py
+++ exercise-02/calc.py
@@ -1,6 +1,29 @@
-print("Enter number 1: ")
-num1 = int(input())
-print("Enter number 2: ")
-num2 = int(input())
-print("Sum: " + str(num1+num2))
-print("Product: " + str(num1*num2))
+def rechnen():
+    print("Enter number 1: ")
+    num1 = int(input())
+    print("Enter number 2: ")
+    num2 = int(input())
+    print("Which kind of mafs operation do you wanna use?")
+    op = str(input())
+
+    if op == "sum"or op =="add" or op =="+":
+        print("Sum: " + str(num1+num2))
+
+    elif op =="product"or op =="multiplication" or op =="*" or op =="multi" or op == "x":
+        print("Product: " + str(num1*num2))
+
+    else:
+        print("Error bibabibup")
+
+    print("Wanna do it again ?")
+
+    lw = input().lower()
+    
+    if lw == "true" or lw =="yes"or lw =="y":
+        rechnen()
+
+    else:
+        quit()
+
+rechnen()
+
