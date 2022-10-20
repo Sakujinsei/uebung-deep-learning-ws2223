1ca613c4f26d238825b78ff9a71499a0daeaf8ae
Dwennimmen <amini_l2001@yahoo.de>
2022-10-20 13:21:40 +0200

    exercise mostly done but not working on my mac

--- exercise-02/calc.py
+++ exercise-02/calc.py
@@ -2,5 +2,9 @@ print("Enter number 1: ")
 num1 = int(input())
 print("Enter number 2: ")
 num2 = int(input())
-print("Sum: " + str(num1+num2))
-print("Product: " + str(num1*num2))
+print("What operation do you want to do? (type \"add\" for \"addition\" or \"multi\" for \"multiplication\")")
+op = input()
+if op == "add":
+    print("You chose "+op+". Sum: " + str(num1+num2))
+else:
+    print("You chose "+op+". Product: " + str(num1*num2))
