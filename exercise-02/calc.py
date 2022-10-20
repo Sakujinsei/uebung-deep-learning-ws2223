58a97177acb2bad7f3eeaf6d058b541a5b102fff
Jacek Jackowski <jacekjackowski@Jans-MacBook-Air.local>
2022-10-20 14:18:44 +0200

    i tried

--- exercise-02/calc.py
+++ exercise-02/calc.py
@@ -1,6 +1,22 @@
-print("Enter number 1: ")
-num1 = int(input())
-print("Enter number 2: ")
-num2 = int(input())
-print("Sum: " + str(num1+num2))
-print("Product: " + str(num1*num2))
+while True:
+    print("Do you want to add or multiply?")
+    input = str(input())
+    print("Enter number 1: ")
+    num1 = int(input())
+    print("Enter number 2: ")
+    num2 = int(input())
+
+    if input == add:
+        print("Sum: " + str(num1+num2))
+
+    elif input == multiply:
+        print("Product: " + str(num1*num2))
+
+    print("Do you want to continue?")
+    input2 = str(input())
+
+    if input2 == yes:
+        continue
+    elif input == no:
+        break
+    
