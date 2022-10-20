92e0fc14759507dd3e069c75b4ba74b824e8a621
BMusolff <92873072+BMusolff@users.noreply.github.com>
2022-10-20 14:31:38 +0200

    Update calc.py

--- exercise-02/calc.py
+++ exercise-02/calc.py
@@ -1,6 +1,24 @@
+import calc
+
+print("add/addition or multi/multiplication")
+option = str(input())
 print("Enter number 1: ")
 num1 = int(input())
 print("Enter number 2: ")
 num2 = int(input())
-print("Sum: " + str(num1+num2))
-print("Product: " + str(num1*num2))
+if option == "add":
+        print("Sum: " + str(num1 + num2))
+elif option == "addition":
+        print("Sum: " + str(num1 + num2))
+elif option == "multi":
+        print("Product: " + str(num1 * num2))
+elif option == "multiplication":
+        print("Product: " + str(num1 * num2))
+print("would you like to continue?")
+option2 = str(input())
+if option2 == "yes":
+    calc
+elif option2 == "continue":
+    calc
+else:
+    exit()
