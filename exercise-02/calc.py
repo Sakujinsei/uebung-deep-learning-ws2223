bf864d7f0d38590bca1442eb864621c22a32260a
Sophie Jorns <sophiejorns@Macbook-Air-Sophie.fritz.box>
2022-10-20 21:06:30 +0200

    finish exercise

--- exercise-02/calc.py
+++ exercise-02/calc.py
@@ -2,5 +2,28 @@ print("Enter number 1: ")
 num1 = int(input())
 print("Enter number 2: ")
 num2 = int(input())
-print("Sum: " + str(num1+num2))
-print("Product: " + str(num1*num2))
+print("Choose an Operator (+,*): ")
+ch = input()
+if ch=='+':
+    print("\n" +str(num1)+ " + " + str(num2)+ " = " +str(num1+num2))
+elif ch=='*':
+    print("\n" +str(num1)+ " * " + str(num2)+ " = " +str(num1*num2))  
+ 
+
+print("Would you like to continue? Y/N")
+co = input()
+if co=='Y':
+    print("Ok, so here we go again!")
+    print("Enter number 1: ")
+    num1 = int(input())
+    print("Enter number 2: ")
+    num2 = int(input())
+    print("Choose an Operator (+,*): ")
+    ch = input()
+    if ch=='+':
+        print("\n" +str(num1)+ " + " + str(num2)+ " = " +str(num1+num2))
+    elif ch=='*':
+        print("\n" +str(num1)+ " * " + str(num2)+ " = " +str(num1*num2))
+        
+elif co=='N':
+    print("Ok, Bye!")
