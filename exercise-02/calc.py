c651208d97a6028109395668f81bfe35e465ff45
Steffi S.-E <steffix317@gmail.com>
2022-10-21 11:54:14 +0200

    added math operation

--- exercise-02/calc.py
+++ exercise-02/calc.py
@@ -1,6 +1,18 @@
-print("Enter number 1: ")
-num1 = int(input())
-print("Enter number 2: ")
-num2 = int(input())
-print("Sum: " + str(num1+num2))
-print("Product: " + str(num1*num2))
+s = "yes"
+while s == "yes":
+    print("Enter number 1: ")
+    num1 = int(input())
+    print("Enter number 2: ")
+    num2 = int(input())
+    print("Enter a math operation add/multiply: ")
+    calc = input()
+
+    if calc == "add" or calc == "+" or calc == "addition":
+        sumAdd = num1 + num2
+        print("Sum: " + str(sumAdd))
+    elif calc == "multi" or calc == "*" or calc == "multiply":
+        sumMulti = num1 * num2
+        print("Product: " + str(sumMulti))
+
+    print("Do you want to play again? Type: yes or no")
+    s = input()
