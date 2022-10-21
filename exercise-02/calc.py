7a649c5b6e0b255b73acd3c489be6162bfd96a64
Cecilia Lauffer <cecilia.lauffer@gmail.com>
2022-10-21 10:28:15 +0200

    HA fertig

--- exercise-02/calc.py
+++ exercise-02/calc.py
@@ -1,6 +1,22 @@
-print("Enter number 1: ")
-num1 = int(input())
-print("Enter number 2: ")
-num2 = int(input())
-print("Sum: " + str(num1+num2))
-print("Product: " + str(num1*num2))
+def calculator():
+    print("Enter number 1: ")
+    num1 = int(input())
+    print("Enter number 2: ")
+    num2 = int(input())
+
+    print("Choose operation: addition or multiplication")
+    op = input()
+    if op == "add" or op == "addition":
+        print("Sum: " + str(num1+num2))
+    elif op == "multi" or op == "multiplication":
+        print("Product: " + str(num1*num2))
+    else:
+        print("invalid input")
+    print("Do you want to continue? type yes or no")
+    if input() == "yes":
+        calculator()
+    else:
+        exit()
+
+
+calculator()
