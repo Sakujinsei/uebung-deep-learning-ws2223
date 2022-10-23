8ffeed72056d197842cb0e6c81b45239e9f7cf1f
Jacek Jackowski <jacekjackowski@jans-air.speedport.ip>
2022-10-23 15:19:18 +0200

    fixed, hopefully

--- exercise-02/calc.py
+++ exercise-02/calc.py
@@ -1,22 +1,31 @@
+add = 'add'
+multiply = 'multiply'
+yes = 'yes'
+no = 'no'
+
 while True:
     print("Do you want to add or multiply?")
-    input = str(input())
+    input1 = str(input())
     print("Enter number 1: ")
     num1 = int(input())
     print("Enter number 2: ")
     num2 = int(input())
 
-    if input == add:
+    if input1 == add:
         print("Sum: " + str(num1+num2))
 
-    elif input == multiply:
+    elif input1 == multiply:
         print("Product: " + str(num1*num2))
+
+    else:
+        print("Invalid input! Try 'add' or 'multiply'.")
+        break
 
     print("Do you want to continue?")
     input2 = str(input())
 
     if input2 == yes:
         continue
-    elif input == no:
+    elif input2 == no:
         break
     
