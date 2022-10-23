17c3502bebea9d0dbec7eb2be383d5c8493b8f14
Your Name <you@example.com>
2022-10-23 18:27:15 +0200

    Homework done

--- exercise-02/calc.py
+++ exercise-02/calc.py
@@ -1,6 +1,21 @@
-print("Enter number 1: ")
-num1 = int(input())
-print("Enter number 2: ")
-num2 = int(input())
-print("Sum: " + str(num1+num2))
-print("Product: " + str(num1*num2))
+row = None
+while row is None:
+    nummer1 = int(input("Enter number 1: "))
+    print(nummer1)
+    nummer2 = int(input("Enter number 2: "))
+    print(nummer2)
+
+    print("Which operation do you want me to do? ")
+    operation = input("Enter + / * or addition / multiplication: ")
+
+    summe = nummer1 + nummer2
+    produkt = nummer1 * nummer2
+
+    if operation == "+" or operation == "addition":
+        print(f'The sum is {summe}.')
+    elif operation == "*" or operation == "multiplication":
+        print(f'The product is {produkt}')
+
+    rep = input("Do you wish to continue? y/n: ")
+    if rep == "n":
+        break
