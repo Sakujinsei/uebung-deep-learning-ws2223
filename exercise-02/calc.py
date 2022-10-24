25ccdf43c7a77f29277d02fc4f23fd435e69ea12
xsandyx <103143981+xsandyx@users.noreply.github.com>
2022-10-24 09:14:58 +0200

    exercise completed

--- exercise-02/calc.py
+++ exercise-02/calc.py
@@ -1,6 +1,15 @@
-print("Enter number 1: ")
-num1 = int(input())
-print("Enter number 2: ")
-num2 = int(input())
-print("Sum: " + str(num1+num2))
-print("Product: " + str(num1*num2))
+conti = "yes"
+while conti == "yes":
+    print("Enter number 1: ")
+    num1 = int(input())
+    print("Enter number 2: ")
+    num2 = int(input())
+    print("Enter mathematical operation (addition or multiplication)")
+    operation = input()
+
+    if operation == "multiply" or operation == "*" or operation == "multi":
+        print("Product:" + str(num1 * num2))
+    if operation == "add" or operation == "+" or operation == "addition":
+        print("Sum: " + str(num1 + num2))
+    print("Would you like to continue? Please type yes or no:")
+    conti = input()
