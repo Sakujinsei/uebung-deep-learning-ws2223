1dc587f52e698673ddb70c8db148259bddd8c685
bjenne <bjenne@smail.uni-koeln.de>
2022-10-24 17:04:35 +0200

    changed

--- exercise-02/calc.py
+++ exercise-02/calc.py
@@ -1,3 +1,4 @@
+
 def func(sum): + str(num1+num2)
 def func(pro): + str(num1*num2)
 
@@ -6,26 +7,28 @@ start: print("Enter number 1: ")
 num1 = int(input())
 print("Enter number 2: ")
 num2 = int(input())
-give = input("sum or pro")
-if sum:
-    print("sum: " + str(num1+num2))
-elif pro:
-    print("pro: " + str(num1*num2))
+print("sum or pro ?")
+give = input()
+if give == "sum":
+     print("sum: " + str(num1+num2))
+if give == "pro":
+     print("pro: " + str(num1*num2))
+answer = input('you want to exit?: [y/n]')
 
-answer = input ('you want to exit?: [y/n]')
-if answer[0].lower() == 'n':
+while answer[0].lower() == 'n':
     print("Restart")
     print("Enter number 1: ")
     num1 = int(input())
     print("Enter number 2: ")
     num2 = int(input())
-    give = input("sum or pro")
-    if sum:
+    print("sum or pro ?")
+    give = input()
+    if give == "sum":
         print("sum: " + str(num1+num2))
-    elif pro:
+    if give == "pro":
         print("pro: " + str(num1*num2))
+    answer = input('you want to exit?: [y/n]')
 
 if answer[0].lower == 'y':
     exit()
-# for yes in range(0, 0):
- #   print("sorry" + exit())
+
