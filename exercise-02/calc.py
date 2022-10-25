22d04cd1ac41dc2e4a6a543569628380794d1680
JohannonBlake <johannon.blade@Gmail.com>
2022-10-25 14:16:02 +0200

    first stable versiono
    Ok, jetzt wo ich sehe wie man ein shutdown command benutzt funktioniert das ganze auch wieder. Jetzt muss ich nur noch die Rekursion hinkriegen

--- exercise-02/calc.py
+++ exercise-02/calc.py
@@ -1,16 +1,17 @@
-
-print("Enter number 1: ")
+print("Enter number 1:")
 num1 = int(input())
-print("Enter number 2: ")
+print("Enter number 2:")
 num2 = int(input())
-print ("what should I do with these numbers?")
-operation = str(input())
-if operation == "add":
+print ("what should I do with these numbers?: ")
+op = str(input())
+if op == "add":
  print("Sum: " + str(num1+num2))
-else: pass 
-if operation == "multiply":
+elif op == "multiply":
  print("Product: " + str(num1*num2))
-else: pass
-
-
-
+else:
+ print("I´m sorry Dave, I can´t do that")
+print("Press Enter to exit")
+a = input()
+if a:
+ exit(0)
+ 
