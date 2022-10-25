3e2665da100260a3ef9e5690922c32f29ce6bd8c
JohannonBlake <johannon.blade@Gmail.com>
2022-10-25 12:55:34 +0200

    brought it over
    noch nicht fertig, aber so hab ich wenigstens die Datei schon mal da, und ne Abgabe falls ich es doch nicht hinkriege

---
+++ exercise-02/calc.py
@@ -0,0 +1,16 @@
+
+print("Enter number 1: ")
+num1 = int(input())
+print("Enter number 2: ")
+num2 = int(input())
+print ("what should I do with these numbers?")
+operation = str(input())
+if operation == "add":
+ print("Sum: " + str(num1+num2))
+else: pass 
+if operation == "multiply":
+ print("Product: " + str(num1*num2))
+else: pass
+
+
+
