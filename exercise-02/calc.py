a4e63abd7c68181df6d54ebe254f1f2d72a18fe4
FStupp <fstupp@smail.uni-koeln.de>
2022-10-26 18:04:23 +0200

    Aufgabe fertig

--- exercise-02/calc.py
+++ exercise-02/calc.py
@@ -1,6 +1,15 @@
-print("Enter number 1: ")
-num1 = int(input())
-print("Enter number 2: ")
-num2 = int(input())
-print("Sum: " + str(num1+num2))
-print("Product: " + str(num1*num2))
+x= "yes"
+while x == "yes":
+    print("Enter number 1: ")
+    num1 = int(input())
+    print("Enter number 2: ")
+    num2 = int(input())
+    print("multiply or add?: ")
+    operation = input()
+
+    if(operation == "multiply" or operation == "multi" or operation == "*"):
+        print("Product: ", str(num1*num2))
+    if(operation == "add" or operation == "addition" or operation == "+"):
+        print("Sum: " + str(num1+num2))
+    print("want to continue? yes/no: ")
+    x = input()
