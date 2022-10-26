2491acaed5fcc1109551f31d1d62e8be4749d46c
Till Pussel <till.pussel@t-online.de>
2022-10-26 19:09:07 +0200

    02 done!

--- exercise-02/calc.py
+++ exercise-02/calc.py
@@ -1,6 +1,25 @@
-print("Enter number 1: ")
-num1 = int(input())
-print("Enter number 2: ")
-num2 = int(input())
-print("Sum: " + str(num1+num2))
-print("Product: " + str(num1*num2))
+class Calculator:
+
+    def calculate(self):
+        print("which mathematical operation do you want to perform? ")
+        op = ""
+        op = str(input())
+        print("Enter number 1: ")
+        num1 = int(input())
+        print("Enter number 2: ")
+        num2 = int(input())
+        print(op)
+        if (op == '+') or (op == 'addition'):
+            print("Sum: " + str(num1+num2))
+        elif (op == '*') or (op == 'multiplication'):
+            print("Product: " + str(num1*num2))
+        else:
+            print("not valid!")
+        print("would you like to perform another operation?")
+        answer = str(input())
+        print(answer)
+        if answer == 'y':
+            cal.calculate()
+cal = Calculator()
+cal.calculate()
+
