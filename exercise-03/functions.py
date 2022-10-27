89f37cf1ae4e12b586de4a1533049ccf303115bb
David Müller <Dreevid@web.de>
2022-10-27 16:36:23 +0200

    Aufgabe 3 fertig

--- exercise-03/functions.py
+++ exercise-03/functions.py
@@ -1,4 +1,4 @@
-def func1(a, b):
+def func1(a, b): 
   if type(a) == int and type(b) == int:
     return a+b
   if type(a) == str and type(b) == str:
@@ -9,8 +9,19 @@ def func1(a, b):
     return None
   return type(a)
 
-func1(1,2)
-func1("Welt", "Hallo")
-func1(None, None)
-func1(11, "Freunde")
-func1(5.4, 3.6)
+def func2(*arg):
+    numberOfArgs = len(arg)
+    print("Laenge von Arg: ", numberOfArgs)
+    if numberOfArgs < 2:
+        return "Number of Arguments " + numberOfArgs
+    if numberOfArgs > 2:
+        return numberOfArgs
+    if numberOfArgs == 2:
+        return func1(arg[0], arg[1])
+
+print(func1(1,2))
+print(func1("Welt", "Hallo"))
+print(func1(None, None))
+print(func1(11, "Freunde"))
+print(func1(5.4, 3.6))
+print(func2(6, 5))
--- exercise-03/listcomprehension.py
+++ exercise-03/listcomprehension.py
@@ -1,3 +1,24 @@
+
 x = [0,1,2,3,4,5,6,7,8,9]
 y = [True, False, False, True, "False", False, True, True, True, "True"]
 
+a = [x*2 for x in x]
+print(a)
+
+b = [x for x in x if x%2==0]
+print(b)
+
+c = [y for y in y if y==True]
+print(c)
+
+d = [y for y in y if isinstance(y,str)]
+print(d)
+
+e = [[True for z in range(x-1)] for x in x if x%2==1]
+print(e)
+
+#- A list called `a` that contains all elements of `x`, multiplied by 2.
+#- A list called `b` that contains all even elements of `x`.
+#- A list called `c` that contains all truish elements of `y` (i.e., all elements that evaluate to `True` in a boolean context)
+#- A list called `d` that contains all string elements of `y`
+#- A list called `e` that contains a list for each odd element of `x`. The number of list elements is given by the current number of `x`, and all inner list elements are `True`. Use I.e., the list `e` starts like this: `[[], [True, True], [True, True, True, True], ...]`
