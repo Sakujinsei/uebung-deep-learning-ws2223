af8ee1450b85a2cc246eb96f5b84903c12130ebe
Dwennimmen <amini_l2001@yahoo.de>
2022-10-27 13:09:14 +0200

    List Comprehension done

--- exercise-03/listcomprehension.py
+++ exercise-03/listcomprehension.py
@@ -1,3 +1,14 @@
 x = [0,1,2,3,4,5,6,7,8,9]
 y = [True, False, False, True, "False", False, True, True, True, "True"]
 
+a = [x*2 for x in x]
+b = [x for x in x if x%2==0]
+c = [y for y in y if y is True]
+d = [y for y in y if isinstance(y, str)]
+e = [[True]*x for x in x if x%2!=0]
+
+# print(a)
+# print(b)
+# print(c)
+# print(d)
+# print(e)
