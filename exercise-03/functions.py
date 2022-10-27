8ecbd0b83ad0281bbe0079b68e9c8cba1c9f4432
whummel <wolf-andre_hummel@live.de>
2022-10-27 13:19:19 +0200

    Finished exercise-03.

--- exercise-03/functions.py
+++ exercise-03/functions.py
@@ -14,3 +14,26 @@ func1("Welt", "Hallo")
 func1(None, None)
 func1(11, "Freunde")
 func1(5.4, 3.6)
+
+def func2(*args):
+    if (len(args) < 2):
+        return f"There are {len(args)} arguments. "
+    if (len(args) == 2):
+        func1(**args)
+    if (len(args) > 2):
+        return len(args)
+
+
+def func3(**kwargs):
+    if (kwargs.get('a') and kwargs.get('b')):
+        func1(**kwargs)
+    else:
+        func2(kwargs.values())
+
+func2(2)
+
+func2(2,3,4,5)
+
+func3(a=3, b=5)
+func3(s=3, c=5)
+
--- exercise-03/listcomprehension.py
+++ exercise-03/listcomprehension.py
@@ -1,3 +1,12 @@
 x = [0,1,2,3,4,5,6,7,8,9]
 y = [True, False, False, True, "False", False, True, True, True, "True"]
 
+a = [i * 2 for i in x]
+b = [i for i in x if i%2 == 0]
+
+c = [n for n in y if n]
+d = [n for n in y if isinstance(n, str)]
+
+e = [i*[bool(i)] for i in x if not i%2==0]
+
+print(f"a = {a}\nb = {b}\nc = {c}\nd = {d}\ne = {e}\n")
