f1e5ea70c80b7c905bc557a8983b87f56bc16af8
JohannonBlake <johannon.blade@Gmail.com>
2022-10-27 13:16:23 +0200

    gimme a second
    somehow I got conflicts without doing somethin

--- exercise-03/listcomprehension.py
+++ exercise-03/listcomprehension.py
@@ -1,3 +1,11 @@
 x = [0,1,2,3,4,5,6,7,8,9]
 y = [True, False, False, True, "False", False, True, True, True, "True"]
 
+a = [x*2 for x in  a]
+print (a)
+
+print ("press enter to exit")
+egress = (input())
+if egress:
+ exit(0)
+
