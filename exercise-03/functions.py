dfbc54cdf9af20ee9ca0031e5fe87dffd1ad5ff1
DKramer8 <103896137+DKramer8@users.noreply.github.com>
2022-10-27 14:48:59 +0200

    func3 in functions.py hoffentlich so richtig...

--- exercise-03/functions.py
+++ exercise-03/functions.py
@@ -9,8 +9,32 @@ def func1(a, b):
     return None
   return type(a)
 
+def func2(*args):
+	if len(args) < 2:
+		return str(len(args))
+	elif len(args) == 2:
+		func1(args[0], args[1])
+	else:
+		return len(args)
+
+def func3(**kwargs):
+	l = [kwargs[key] for key in kwargs]
+	ab = [ab for ab in l if ab == "a" or ab == "b"]
+	if len(ab) == 2:
+		func1(ab[0], ab[1])
+	else:
+		func2(l)
+
 func1(1,2)
 func1("Welt", "Hallo")
 func1(None, None)
 func1(11, "Freunde")
 func1(5.4, 3.6)
+
+func2(3, 6, 4)
+func2(3,7)
+func2("Hans")
+
+func3(a="a", b="b", c="b")
+func3(a="n")
+func3(a="a", b="b")
--- exercise-03/listcomprehension.py
+++ exercise-03/listcomprehension.py
@@ -1,3 +1,18 @@
 x = [0,1,2,3,4,5,6,7,8,9]
 y = [True, False, False, True, "False", False, True, True, True, "True"]
 
+a = [el*2 for el in x]
+print(a)
+
+b = [el for el in x if el % 2 == 0]
+print(b)
+
+c = [el for el in y if el == True]
+print(c)
+
+d = [el for el in y if isinstance(el, str) == True]
+print(d)
+
+e = [[True for innerEl in range(el)] for el in x if el % 2 != 0]
+print(e)
+
