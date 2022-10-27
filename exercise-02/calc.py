1870fc8a996b6c756634de47fecd99bb9d8330e3
laryrauh <laryrauh@localhost>
2022-10-27 10:49:10 +0200

    Hausaufgaben

--- exercise-02/calc.py
+++ exercise-02/calc.py
@@ -2,5 +2,8 @@ print("Enter number 1: ")
 num1 = int(input())
 print("Enter number 2: ")
 num2 = int(input())
-print("Sum: " + str(num1+num2))
-print("Product: " + str(num1*num2))
+operator = input("Addition or multiplication? Please type in add or multi:")
+if (operator == "add"):
+    print("Sum: " + str(num1+num2))
+elif (operator == "multi"):
+    print("Product: " + str(num1*num2))
