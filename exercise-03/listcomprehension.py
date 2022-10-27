e50f054f1aede0b4a1017d2d7f1d03b85377418a
JohannonBlake <johannon.blade@Gmail.com>
2022-10-27 13:20:48 +0200

    jlkj
    didn´t really do anything, just gotta get to the thing on github

--- exercise-03/listcomprehension.py
+++ exercise-03/listcomprehension.py
@@ -1,10 +1,10 @@
 x = [0,1,2,3,4,5,6,7,8,9]
 y = [True, False, False, True, "False", False, True, True, True, "True"]
 
-a = [x*2 for x in  a]
+a = [x*2 for x in a]
 print (a)
 
-print ("press enter to exit")
+print("press enter to exit")
 egress = (input())
 if egress:
  exit(0)
