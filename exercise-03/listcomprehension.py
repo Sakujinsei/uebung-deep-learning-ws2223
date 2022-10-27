e118042826a3ce0ded40eed7d00a0ad79a6e5614
BMusolff <92873072+BMusolff@users.noreply.github.com>
2022-10-27 14:46:33 +0200

    Exercise 3: Step 1 finished

--- exercise-03/listcomprehension.py
+++ exercise-03/listcomprehension.py
@@ -1,3 +1,17 @@
 x = [0,1,2,3,4,5,6,7,8,9]
 y = [True, False, False, True, "False", False, True, True, True, "True"]
 
+a = [z*2 for z in x]
+print(a)
+
+b = [z for z in x if z%2 == 1]
+print(b)
+
+c = [z for z in y if z == True]
+print(c)
+
+d = [z for z in y]
+print(d)
+
+e = [z*[bool(z)] for z in b]
+print(e)
