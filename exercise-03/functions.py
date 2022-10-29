8ff1558242fe63c22b15f55e549eba539893e309
xsandyx <103143981+xsandyx@users.noreply.github.com>
2022-10-29 10:12:42 +0200

    exercise completed

--- exercise-03/functions.py
+++ exercise-03/functions.py
@@ -1,16 +1,33 @@
 def func1(a, b):
-  if type(a) == int and type(b) == int:
-    return a+b
-  if type(a) == str and type(b) == str:
-    return b+" "+a
-  if a is None and b is None:
-    return "Does not exist."
-  if type(a) != type(b):
-    return None
-  return type(a)
+    if type(a) == int and type(b) == int:
+        return a + b
+    if type(a) == str and type(b) == str:
+        return b + " " + a
+    if a is None and b is None:
+        return "Does not exist."
+    if type(a) != type(b):
+        return None
+    return type(a)
 
-func1(1,2)
+
+func1(1, 2)
 func1("Welt", "Hallo")
 func1(None, None)
 func1(11, "Freunde")
 func1(5.4, 3.6)
+
+
+def func2(*args):
+    if len(args) == 2:
+        return func1(args[0], args[1])
+    elif len(args) < 2:
+        return str(len(args))
+    else:
+        return len(args)
+
+
+def func3(**kwargs):
+    if ["a", "b"] in kwargs.keys():
+        return func1(kwargs.get("a"), kwargs.get("b"))
+    else:
+        return func2(kwargs.keys())
--- exercise-03/listcomprehension.py
+++ exercise-03/listcomprehension.py
@@ -1,3 +1,17 @@
 x = [0,1,2,3,4,5,6,7,8,9]
 y = [True, False, False, True, "False", False, True, True, True, "True"]
 
+a = [x*2 for x in x]
+print(a)
+
+b = [x for x in x if x % 2 == 0]
+print(b)
+
+c = [y for y in y if y == True]
+print(c)
+
+d = [x for x in y if isinstance(x, str)]
+print(d)
+
+e = [[True for x2 in range(x)] for x in x]
+print(e)
