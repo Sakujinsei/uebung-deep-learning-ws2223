925e8cd7cb582bc8b1d0d8a09c9443e1e6be09c6
JohannonBlake <johannon.blade@Gmail.com>
2022-11-02 13:17:29 +0100

    Functions 3/3

--- exercise-03/functions.py
+++ exercise-03/functions.py
@@ -1,3 +1,8 @@
+from asyncio.windows_events import NULL
+from calendar import c
+from contextlib import nullcontext
+
+
 def func1(a, b):
   if type(a) == int and type(b) == int:
     return a+b
@@ -14,3 +19,39 @@ func1("Welt", "Hallo")
 func1(None, None)
 func1(11, "Freunde")
 func1(5.4, 3.6)
+
+def func2():
+  print ("Hello, give me a few things you would like to have added. Press the q key when you are done")
+  c = list( input().split())
+ 
+  if len(c) < 2:
+     return(len(c))
+  elif len(c) == 2:
+    func1(*c)
+  elif len(c) > 2:
+    return(len(c))
+  else:
+    return(None)
+
+func2()
+
+def func3():
+
+ d =	{
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "a": 4,
+  "b": 7
+}
+ if "a" in d and "b" in d:
+  func1("a","b")
+ else:
+  func2(*d)
+
+
+
+print("press enter to exit")
+egress = input()
+if egress:
+ exit(0)
