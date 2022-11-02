c09077e9ffe7048db5910ce9aefc4ae84bafcc1a
FStupp <fstupp@smail.uni-koeln.de>
2022-11-02 16:01:38 +0100

    Aufgabe fertig

--- exercise-03/listcomprehension.py
+++ exercise-03/listcomprehension.py
@@ -1,3 +1,17 @@
 x = [0,1,2,3,4,5,6,7,8,9]
 y = [True, False, False, True, "False", False, True, True, True, "True"]
 
+a = [x*2 for x in x]
+print(a)
+
+b = [x for x in x if x%2==0]
+print(b)
+
+c = [y for y in y if y==True]
+print(c)
+
+d = [y for y in y if isinstance(y,str)]
+print(d)
+
+e = [[True for z in range(x-1)] for x in x if x%2==1]
+print(e)
