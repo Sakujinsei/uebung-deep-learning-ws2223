30b15df0a6cb594e466fefc33bdb6eef0c144001
sarami <sara.unimail@gmail.com>
2022-11-02 16:22:52 +0100

    Changes made to functions and listcomprehension

--- exercise-03/functions.py
+++ exercise-03/functions.py
@@ -14,3 +14,23 @@ func1("Welt", "Hallo")
 func1(None, None)
 func1(11, "Freunde")
 func1(5.4, 3.6)
+
+def func2(*args):
+  if len(args) <= 2:
+    return str(len(args))
+  if len(args) == 2:
+    func1(args)
+  else:
+    return int(len(args))
+
+print(func2("miau", "si", "no"))
+print(func2(5))
+print(func2(5, 5))
+
+def func3(*args):
+  if args.name == "a" and "b":
+    func1()
+  else:
+    func2()
+
+print(func3(a="Katze", b="Bobby"))
--- exercise-03/listcomprehension.py
+++ exercise-03/listcomprehension.py
@@ -1,3 +1,13 @@
 x = [0,1,2,3,4,5,6,7,8,9]
 y = [True, False, False, True, "False", False, True, True, True, "True"]
 
+a = [x*2 for x in x]
+print(a)
+b = [x for x in x if x%2 == 0]
+print(b)
+c = [ x for x in y if x == True]
+print(c)
+d = [x for x in y if type(x) == str]
+print(d)
+e = [x for x in x if x%2 != 0]
+print(e)
